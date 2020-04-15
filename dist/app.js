f4622d25c955baa28051bf169bc87be55177ef17
Jared Vandeventer <vanthedev@gmail.com>
2020-04-15 17:24:17 -0600

    add base component class

--- dist/app.js
+++ dist/app.js
@@ -84,6 +84,22 @@ var SingleProject = (function () {
     }
     return SingleProject;
 }());
+var Component = (function () {
+    function Component(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = (document.getElementById(templateId));
+        this.hostElement = document.getElementById(hostElementId);
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
+    }
+    Component.prototype.attach = function (insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+    };
+    return Component;
+}());
 var ProjectList = (function () {
     function ProjectList(type) {
         var _this = this;
--- src/app.ts
+++ src/app.ts
@@ -114,6 +114,39 @@ class SingleProject {
 }
 // Single Project Class
 
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById(templateId)
+    );
+    this.hostElement = document.getElementById(hostElementId) as T;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+    this.attach(insertAtStart);
+  }
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
+
 // ProjectList Class
 class ProjectList {
   templateElement: HTMLTemplateElement;
